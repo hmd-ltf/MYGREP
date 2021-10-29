bff2db2b28890787dd51ddd3a163e3ad2ab55cce
hmd-ltf <47081452+hmd-ltf@users.noreply.github.com>
2021-10-29 11:30:42 +0500

    can get args and read file

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {} in {}" ,query , filename);
+
+    let contents = fs::read_to_string(filename).expect("File not Found");
+    println!("{}" , contents);
+}
