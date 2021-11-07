a9fd9affb34f514b99b8a84ed0497c2c1c964113
hmd-ltf <47081452+hmd-ltf@users.noreply.github.com>
2021-11-07 15:25:25 +0500

    Added ENV variables to control case sensative

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub fn run(config: Config) -> Result<() , Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query , &contents) {
+    let result = if config.is_case_sensitive {
+        search(&config.query , &contents)
+    } else {
+        case_insensative_search(&config.query , &contents)
+    };
+
+    for line in result{
         println!("{}" , line);
     }
     Ok(())
@@ -12,6 +19,7 @@ pub fn run(config: Config) -> Result<() , Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub is_case_sensitive: bool,
 }
 
 impl Config{
@@ -21,7 +29,8 @@ impl Config{
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok (Config {query , filename})
+        let is_case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok (Config {query , filename, is_case_sensitive})
  }
 }
 
@@ -33,6 +42,19 @@ pub fn search<'a>(query: &str , content: &'a str) -> Vec<&'a str> {
             results.push(line);
         }
     }
+    results
+}
+pub fn case_insensative_search<'a>(query: &str , content: &'a str)-> Vec<&'a str> {
+    
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+
     results
 }
 
@@ -49,4 +71,29 @@ safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."] , search(query, content));
     }
+
+    #[test]
+    fn sensative() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+DUCT SOmeTHING.";
+        assert_eq!(vec!["safe, fast, productive."] , search(query , content));
+    }
+
+    #[test]
+    fn in_sensative() {
+        let query = "RusT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust ME BRO";
+        assert_eq!(vec!["Rust:", "Trust ME BRO"] ,case_insensative_search(query , content));
+    }
+
+
+
 }
